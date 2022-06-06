fcc73c5c8ce661f4c207c53595aa7e7ceb16638d
WakanaIdeta <k021g1162@g.neec.ac.jp>
2022-06-06 15:45:38 +0900

    組み合わせできた

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -34,7 +34,7 @@ void GameScene::Initialize() {
 	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 
 	//x,y,z方向のスケーリングを設定
-	worldtransform_.scale_ = { 1,10,1 };
+	worldtransform_.scale_ = { 5,5,5 };
 
 	//スケーリング倍率を行列を宣言
 	Matrix4 matScale;
@@ -44,87 +44,87 @@ void GameScene::Initialize() {
 	matScale.m[2][2] = worldtransform_.scale_.z;
 	matScale.m[3][3] = 1;
 
-	worldtransform_.matWorld_.m[0][0] = 1;
-	worldtransform_.matWorld_.m[1][1] = 1;
-	worldtransform_.matWorld_.m[2][2] = 1;
-	worldtransform_.matWorld_.m[3][3] = 1;
+	//worldtransform_.matWorld_.m[0][0] = 1;
+	//worldtransform_.matWorld_.m[1][1] = 1;
+	//worldtransform_.matWorld_.m[2][2] = 1;
+	//worldtransform_.matWorld_.m[3][3] = 1;
 
-	worldtransform_.matWorld_ *= matScale;
+	//worldtransform_.matWorld_.operator*=(matScale);
 
-	//行列の転送
-	worldtransform_.TransferMatrix();
+	////行列の転送
+	//worldtransform_.TransferMatrix();
 
-	//x,y,z軸周りの回転角を設定
-	worldtransform_.rotation_ = { 0.0f,0.0f,radian(45.0f)};
+	////x,y,z軸周りの回転角を設定
+	//worldtransform_.rotation_ = { 0.0f,0.0f,radian(45.0f)};
 
-	//z軸回転行列を宣言
-	Matrix4 matRotZ;
+	////z軸回転行列を宣言
+	//Matrix4 matRotZ;
 
-	matRotZ.m[0][0] = cos(radian(45.0f));
-	matRotZ.m[0][1] = sin(radian(45.0f));
-	matRotZ.m[1][0] = -sin(radian(45.0f));
-	matRotZ.m[1][1] = cos(radian(45.0f));
-	matRotZ.m[2][2] = 1;
-	matRotZ.m[3][3] = 1;
+	//matRotZ.m[0][0] = cos(radian(45.0f));
+	//matRotZ.m[0][1] = sin(radian(45.0f));
+	//matRotZ.m[1][0] = -sin(radian(45.0f));
+	//matRotZ.m[1][1] = cos(radian(45.0f));
+	//matRotZ.m[2][2] = 1;
+	//matRotZ.m[3][3] = 1;
 
-	worldtransform_.matWorld_.m[0][0] = 1;
-	worldtransform_.matWorld_.m[1][1] = 1;
-	worldtransform_.matWorld_.m[2][2] = 1;
-	worldtransform_.matWorld_.m[3][3] = 1;
+	//worldtransform_.matWorld_.m[0][0] = 1;
+	//worldtransform_.matWorld_.m[1][1] = 1;
+	//worldtransform_.matWorld_.m[2][2] = 1;
+	//worldtransform_.matWorld_.m[3][3] = 1;
 
-	worldtransform_.matWorld_ *= matRotZ;
+	//worldtransform_.matWorld_ *= matRotZ;
 
-	//行列の転送
-	worldtransform_.TransferMatrix();
+	////行列の転送
+	//worldtransform_.TransferMatrix();
 
-	//x,y,z軸周りの回転角を設定
-	worldtransform_.rotation_ = { radian(45.0f),0.0f,0.0f };
+	////x,y,z軸周りの回転角を設定
+	//worldtransform_.rotation_ = { radian(45.0f),0.0f,0.0f };
 
-	//x軸回転行列を宣言
-	Matrix4 matRotX;
+	////x軸回転行列を宣言
+	//Matrix4 matRotX;
 
-	matRotX.m[0][0] = 1;
-	matRotX.m[1][1] = cos(radian(45.0f));
-	matRotX.m[1][2] = sin(radian(45.0f));
-	matRotX.m[2][1] = -sin(radian(45.0f));
-	matRotX.m[2][2] = cos(radian(45.0f));
-	matRotX.m[3][3] = 1;
+	//matRotX.m[0][0] = 1;
+	//matRotX.m[1][1] = cos(radian(45.0f));
+	//matRotX.m[1][2] = sin(radian(45.0f));
+	//matRotX.m[2][1] = -sin(radian(45.0f));
+	//matRotX.m[2][2] = cos(radian(45.0f));
+	//matRotX.m[3][3] = 1;
 
-	worldtransform_.matWorld_.m[0][0] = 1;
-	worldtransform_.matWorld_.m[1][1] = 1;
-	worldtransform_.matWorld_.m[2][2] = 1;
-	worldtransform_.matWorld_.m[3][3] = 1;
+	//worldtransform_.matWorld_.m[0][0] = 1;
+	//worldtransform_.matWorld_.m[1][1] = 1;
+	//worldtransform_.matWorld_.m[2][2] = 1;
+	//worldtransform_.matWorld_.m[3][3] = 1;
 
-	worldtransform_.matWorld_ *= matRotX;
+	//worldtransform_.matWorld_.operator*=(matRotX);
 
-	//行列の転送
-	worldtransform_.TransferMatrix();
+	////行列の転送
+	//worldtransform_.TransferMatrix();
 
-	//x,y,z軸周りの回転角を設定
-	worldtransform_.rotation_ = { 0.0f,radian(45.0f),0.0f };
+	////x,y,z軸周りの回転角を設定
+	//worldtransform_.rotation_ = { 0.0f,radian(45.0f),0.0f };
 
-	//y軸回転行列を宣言
-	Matrix4 matRotY;
+	////y軸回転行列を宣言
+	//Matrix4 matRotY;
 
-	matRotY.m[0][0] = cos(radian(45.0f));
-	matRotY.m[0][2] = -sin(radian(45.0f));
-	matRotY.m[1][1] = 1;
-	matRotY.m[2][0] = sin(radian(45.0f));
-	matRotY.m[2][2] = cos(radian(45.0f));
-	matRotY.m[3][3] = 1;
+	//matRotY.m[0][0] = cos(radian(45.0f));
+	//matRotY.m[0][2] = -sin(radian(45.0f));
+	//matRotY.m[1][1] = 1;
+	//matRotY.m[2][0] = sin(radian(45.0f));
+	//matRotY.m[2][2] = cos(radian(45.0f));
+	//matRotY.m[3][3] = 1;
 
-	worldtransform_.matWorld_.m[0][0] = 1;
-	worldtransform_.matWorld_.m[1][1] = 1;
-	worldtransform_.matWorld_.m[2][2] = 1;
-	worldtransform_.matWorld_.m[3][3] = 1;
+	//worldtransform_.matWorld_.m[0][0] = 1;
+	//worldtransform_.matWorld_.m[1][1] = 1;
+	//worldtransform_.matWorld_.m[2][2] = 1;
+	//worldtransform_.matWorld_.m[3][3] = 1;
 
-	worldtransform_.matWorld_ *= matRotY;
+	//worldtransform_.matWorld_.operator*=(matRotY);
 
-	//行列の転送
-	worldtransform_.TransferMatrix();
+	////行列の転送
+	//worldtransform_.TransferMatrix();
 
 	//x,y,z軸周りの平行移動を設定
-	worldtransform_.translation_ = { 0, 10, 0 };
+	worldtransform_.translation_ = { 10, 10, 10 };
 
 	//平行移動行列を宣言
 	Matrix4 matTrans = MathUtility::Matrix4Identity();
@@ -133,15 +133,15 @@ void GameScene::Initialize() {
 	matTrans.m[3][1] = worldtransform_.translation_.y;
 	matTrans.m[3][2] = worldtransform_.translation_.z;
 
-	worldtransform_.matWorld_.m[0][0] = 1;
-	worldtransform_.matWorld_.m[1][1] = 1;
-	worldtransform_.matWorld_.m[2][2] = 1;
-	worldtransform_.matWorld_.m[3][3] = 1;
+	//worldtransform_.matWorld_.m[0][0] = 1;
+	//worldtransform_.matWorld_.m[1][1] = 1;
+	//worldtransform_.matWorld_.m[2][2] = 1;
+	//worldtransform_.matWorld_.m[3][3] = 1;
 
-	worldtransform_.matWorld_ *= matTrans;
+	//worldtransform_.matWorld_.operator*=(matTrans);
 
-	//行列の転送
-	worldtransform_.TransferMatrix();
+	////行列の転送
+	//worldtransform_.TransferMatrix();
 
 	////行列の合成
 	//worldtransform_.matWorld_.m[0][0] = 1;
@@ -154,38 +154,39 @@ void GameScene::Initialize() {
 	//worldtransform_.TransferMatrix();
 
 	////x,y,z軸周りの回転角を設定
-	//worldtransform_.rotation_ = { radian(45.0f), radian(45.0f), radian(45.0f) };
+	worldtransform_.rotation_ = { radian(45.0f), radian(45.0f), radian(0.0f) };
 
 	////合成用回転行列を宣言
-	//Matrix4 matRot;
+	Matrix4 matRot;
 
 	////各軸用回転行列を宣言
-	//Matrix4 matRotX, matRotY, matRotZ;
+	Matrix4 matRotX, matRotY, matRotZ;
 
-	////回転行列の各要素を設定する
-	//matRotX.m[0][0] = 1;
-	//matRotX.m[1][1] = cos(radian(45.0f));
-	//matRotX.m[1][2] = sin(radian(45.0f));
-	//matRotX.m[2][1] = -sin(radian(45.0f));
-	//matRotX.m[2][2] = cos(radian(45.0f));
-	//matRotX.m[3][3] = 1;
+	//回転行列の各要素を設定する
+	matRotX.m[0][0] = 1;
+	matRotX.m[1][1] = cos(radian(45.0f));
+	matRotX.m[1][2] = sin(radian(45.0f));
+	matRotX.m[2][1] = -sin(radian(45.0f));
+	matRotX.m[2][2] = cos(radian(45.0f));
+	matRotX.m[3][3] = 1;
 
-	//matRotY.m[0][0] = cos(radian(45.0f));
-	//matRotY.m[0][2] = -sin(radian(45.0f));
-	//matRotY.m[1][1] = 1;
-	//matRotY.m[2][0] = sin(radian(45.0f));
-	//matRotY.m[2][2] = cos(radian(45.0f));
-	//matRotY.m[3][3] = 1;
+	matRotY.m[0][0] = cos(radian(45.0f));
+	matRotY.m[0][2] = -sin(radian(45.0f));
+	matRotY.m[1][1] = 1;
+	matRotY.m[2][0] = sin(radian(45.0f));
+	matRotY.m[2][2] = cos(radian(45.0f));
+	matRotY.m[3][3] = 1;
 
-	//matRotZ.m[0][0] = cos(radian(45.0f));
-	//matRotZ.m[0][1] = sin(radian(45.0f));
-	//matRotZ.m[1][0] = -sin(radian(45.0f));
-	//matRotZ.m[1][1] = cos(radian(45.0f));
-	//matRotZ.m[2][2] = 1;
-	//matRotZ.m[3][3] = 1;
+	matRotZ.m[0][0] = cos(radian(0.0f));
+	matRotZ.m[0][1] = sin(radian(0.0f));
+	matRotZ.m[1][0] = -sin(radian(0.0f));
+	matRotZ.m[1][1] = cos(radian(0.0f));
+	matRotZ.m[2][2] = 1;
+	matRotZ.m[3][3] = 1;
 
-	////各軸の回転行列を合成
-	//matRot = matRotZ * matRotX * matRotY;
+	//各軸の回転行列を合成
+	Matrix4 mat = matRotZ.operator*=(matRotX);
+	matRot = matRotZ.operator*=(matRotY);
 
 	//worldtransform_.matWorld_.m[0][0] = 1;
 	//worldtransform_.matWorld_.m[1][1] = 1;
@@ -194,8 +195,16 @@ void GameScene::Initialize() {
 
 	//worldtransform_.matWorld_ *= matRot;
 
-	////行列の転送
-	//worldtransform_.TransferMatrix();
+	//行列の合成
+	worldtransform_.matWorld_.m[0][0] = 1;
+	worldtransform_.matWorld_.m[1][1] = 1;
+	worldtransform_.matWorld_.m[2][2] = 1;
+	worldtransform_.matWorld_.m[3][3] = 1;
+
+	worldtransform_.matWorld_ = matScale.operator*=(matRot).operator*=(matTrans);
+
+	//行列の転送
+	worldtransform_.TransferMatrix();
 }
 
 void GameScene::Update() {
