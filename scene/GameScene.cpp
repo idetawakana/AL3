4f9f82b791dc0e18ae8c0f66fdc4973b063dda93
WakanaIdeta <k021g1162@g.neec.ac.jp>
2022-06-06 15:53:18 +0900

    初期設定 ラジアン追加

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -28,6 +28,9 @@ void GameScene::Initialize() {
 
 	//軸方向表示の表示を有効にする
 	AxisIndicator::GetInstance()->SetVisible(true);
+
+	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
 void GameScene::Update() {
