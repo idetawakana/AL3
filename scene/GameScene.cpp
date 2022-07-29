e8d5af4b4e7ad7dc0c3d1b139e65fcb09207b161
WakanaIdeta <k021g1162@g.neec.ac.jp>
2022-07-29 12:00:41 +0900

    totyuu

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -7,6 +7,8 @@
 
 void Determinant(WorldTransform worldtransform_, Vector3 scale, Vector3 rotation, Vector3 translation);
 
+void Determinant_(WorldTransform worldtransform_, Vector3 scale, Vector3 rotation, Vector3 translation);
+
 Matrix4 scale(Vector3 scale);
 
 Matrix4 rotationX(float angle);
@@ -15,7 +17,7 @@ Matrix4 rotationY(float angle);
 
 Matrix4 rotationZ(float angle);
 
-Matrix4 translation(Vector3 trans);
+void translation(WorldTransform worldTransform, Vector3 trans);
 
 WorldTransform world(Matrix4 matScale, Matrix4 matRotX, Matrix4 matRotY, Matrix4 matRotZ, Matrix4 matTrans);
 
@@ -44,9 +46,6 @@ void GameScene::Initialize() {
 	worldTransforms_[1].translation_ = { 0,4.5f,0 };
 	worldTransforms_[1].parent_ = &worldTransforms_[0];
 
-	//アスペクト比を設定
-	viewProjection_.aspectRatio = 1.0f;
-
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 
@@ -67,6 +66,33 @@ void GameScene::Update() {
 		//キャラクターの移動ベクトル
 		Vector3 move = { 0,0,0 };
 
+		if (input_->PushKey(DIK_RIGHT)) {
+			move.x += 1.0;
+		}
+
+		if (input_->PushKey(DIK_LEFT)) {
+			move.x -= 1.0;
+		}
+
+		worldTransforms_[0].translation_ += move;
+
+		translation(worldTransforms_[0], worldTransforms_[0].translation_);
+
+		//デバッグ用表示
+		debugText_->SetPos(50, 110);
+		debugText_->Printf(" %f, %f, %f",
+			worldTransforms_[0].translation_.x,
+			worldTransforms_[0].translation_.y,
+			worldTransforms_[0].translation_.z);
+	}
+
+	//子の更新
+	{
+		Determinant(worldTransforms_[1],{1,1,1}, {0,0,0}, worldTransforms_[1].translation_);
+
+		worldTransforms_[1].matWorld_.operator*=(worldTransforms_[0].matWorld_);
+
+		worldTransforms_[1].TransferMatrix();
 	}
 }
 
@@ -97,9 +123,8 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 	/// 3Dモデル描画
-	for (WorldTransform& worldtransform : worldTransforms_) {
-		model_->Draw(worldtransform, viewProjection_, textureHandle_);
-	}
+	model_->Draw(worldTransforms_[0], viewProjection_, textureHandle_);
+	model_->Draw(worldTransforms_[1], viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -191,6 +216,72 @@ void Determinant(WorldTransform worldtransform_, Vector3 scale, Vector3 rotation
 	worldtransform_.TransferMatrix();
 }
 
+void Determinant_(WorldTransform worldtransform_, Vector3 scale, Vector3 rotation, Vector3 translation) {
+	//x,y,z方向のスケーリングを設定
+	worldtransform_.scale_ = scale;
+
+	//スケーリング倍率を行列を宣言
+	Matrix4 matScale;
+
+	matScale.m[0][0] = worldtransform_.scale_.x;
+	matScale.m[1][1] = worldtransform_.scale_.y;
+	matScale.m[2][2] = worldtransform_.scale_.z;
+	matScale.m[3][3] = 1;
+
+	//x,y,z軸周りの平行移動を設定
+	worldtransform_.translation_ = translation;
+
+	//平行移動行列を宣言
+	Matrix4 matTrans = MathUtility::Matrix4Identity();
+
+	matTrans.m[3][0] = worldtransform_.translation_.x;
+	matTrans.m[3][1] = worldtransform_.translation_.y;
+	matTrans.m[3][2] = worldtransform_.translation_.z;
+
+	////x,y,z軸周りの回転角を設定
+	worldtransform_.rotation_ = rotation;
+
+	////合成用回転行列を宣言
+	Matrix4 matRot;
+
+	////各軸用回転行列を宣言
+	Matrix4 matRotX, matRotY, matRotZ;
+
+	//回転行列の各要素を設定する
+	matRotX.m[0][0] = 1;
+	matRotX.m[1][1] = cos(radian(worldtransform_.rotation_.x));
+	matRotX.m[1][2] = sin(radian(worldtransform_.rotation_.x));
+	matRotX.m[2][1] = -sin(radian(worldtransform_.rotation_.x));
+	matRotX.m[2][2] = cos(radian(worldtransform_.rotation_.x));
+	matRotX.m[3][3] = 1;
+
+	matRotY.m[0][0] = cos(radian(worldtransform_.rotation_.y));
+	matRotY.m[0][2] = -sin(radian(worldtransform_.rotation_.y));
+	matRotY.m[1][1] = 1;
+	matRotY.m[2][0] = sin(radian(worldtransform_.rotation_.y));
+	matRotY.m[2][2] = cos(radian(worldtransform_.rotation_.y));
+	matRotY.m[3][3] = 1;
+
+	matRotZ.m[0][0] = cos(radian(worldtransform_.rotation_.z));
+	matRotZ.m[0][1] = sin(radian(worldtransform_.rotation_.z));
+	matRotZ.m[1][0] = -sin(radian(worldtransform_.rotation_.z));
+	matRotZ.m[1][1] = cos(radian(worldtransform_.rotation_.z));
+	matRotZ.m[2][2] = 1;
+	matRotZ.m[3][3] = 1;
+
+	//各軸の回転行列を合成
+	Matrix4 mat = matRotZ.operator*=(matRotX);
+	matRot = matRotZ.operator*=(matRotY);
+
+	//行列の合成
+	worldtransform_.matWorld_.m[0][0] = 1;
+	worldtransform_.matWorld_.m[1][1] = 1;
+	worldtransform_.matWorld_.m[2][2] = 1;
+	worldtransform_.matWorld_.m[3][3] = 1;
+
+	worldtransform_.matWorld_ = matScale.operator*=(matRot).operator*=(matTrans);
+}
+
 Matrix4 scale(Vector3 scale) {
 	//スケーリング倍率を行列を宣言
 	Matrix4 matScale;
@@ -244,15 +335,18 @@ Matrix4 rotationZ(float angle) {
 	return matRotZ;
 }
 
-Matrix4 translation(Vector3 trans) {
-	//平行移動行列を宣言
+void translation(WorldTransform worldTransform,Vector3 trans) {
 	Matrix4 matTrans = MathUtility::Matrix4Identity();
 
 	matTrans.m[3][0] = trans.x;
 	matTrans.m[3][1] = trans.y;
 	matTrans.m[3][2] = trans.z;
 
-	return matTrans;
+	worldTransform.matWorld_ = MathUtility::Matrix4Identity();
+
+	worldTransform.matWorld_ *= matTrans;
+
+	worldTransform.TransferMatrix();
 }
 
 WorldTransform world(Matrix4 matScale, Matrix4 matRotX, Matrix4 matRotY, Matrix4 matRotZ, Matrix4 matTrans) {
